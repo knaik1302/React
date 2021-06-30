32ffef8aab0b2029a34cdce9b1fa030df8e8f1e0
Kartik V Naik <85819372+knaik1302@users.noreply.github.com>
2021-06-30 12:44:31 +0530

    Add files via upload
    Place this src folder in your react src folder

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import React from "react";
+import './App.css';
+import FetchRandomUser from "./Components/FetchRandomUser";
+
+function App() {
+  return (
+    <FetchRandomUser/>
+  );
+}
+
+export default App;
